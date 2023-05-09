5e123bc5db88efb3917d5c7564fbb30cad4db6cd
Paulo Ricardo Freire Ultra <prfultra@yahoo.com.br>
2023-05-09 11:15:49 -0300

    Lista de Heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,21 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Hero } from '../hero.model';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.scss']
 })
-export class HeroesComponent implements OnInit{
+export class HeroesComponent {
 
   hero: Hero = {
     id: 1,
     name: 'Wolverine'
   };
 
-  constructor() { }
+  heroes = HEROES;
+  //Interrogação indica que a variável pode ser undefined
+  selectedHero?: Hero;
 
-  ngOnInit(): void {
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
-
 }

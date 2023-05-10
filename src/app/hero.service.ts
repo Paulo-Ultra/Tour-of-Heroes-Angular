d871728923e913379a19304ddd1485fc39cbce83
Paulo Ricardo Freire Ultra <prfultra@yahoo.com.br>
2023-05-10 11:43:08 -0300

    Anotações sobre o Observable

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HEROES } from './mock-heroes';
 import { Hero } from './hero.model';
-import { Observable, of } from 'rxjs';
+import { Observable, of, throwError } from 'rxjs';
 
 //Quando inicia a aplicação o service vai ser criado no momento inicial da aplicação
 @Injectable({
@@ -12,6 +12,8 @@ export class HeroService {
   getHeroes(): Observable<Hero[]> {
     //A lista por conta do of da biblioteca rxjs se torna um observable
     const heroes = of(HEROES);
+
+    //return throwError(new Error('Erro ao buscar heróis'));
     return heroes;
   }
 }
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { HeroService } from './../hero.service';
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero.model';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-heroes',
@@ -20,10 +21,60 @@ export class HeroesComponent implements OnInit{
    }
 
    getHeroes(): void {
-    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+    this.heroService.getHeroes().subscribe(
+      (heroes) => (this.heroes = heroes));
    }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
+
+  /*getHeroes(): void {
+    //Para o Observable executar é necessário o subcribe()
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+     //O subscribe() é um método que recebe até três callbacks, que tratam notificações enviadas pelo observable, parecido com try/catch no java
+     this.heroService.getHeroes().subscribe({
+      //Somente esse método é obrigatório
+      next(x) {
+        console.log('got value ' + JSON.stringify(x, null, 2));
+      },
+      error(err) {
+        console.error('something wrong occurred: ' + err);
+      },
+      complete() {
+        console.log('done');
+      },
+    });
+
+    //Com arrow function
+    this.heroService.getHeroes().subscribe(
+      (value) => console.log(value),
+    (error) => console.log(error),
+    ()=> console.log('Agora foi concluído!'));
+
+    //Exemplo da Documentação do rxjs.dev
+    const observable = new Observable((subscriber) => {
+      //Observable pode retornar valores sincronos, assincronos ou ambos e usando o next podemos pegar o valor emitido pelo observable,
+      //antes de passar para o subscribe
+      subscriber.next(1);
+      subscriber.next(2);
+      subscriber.next(3);
+      setTimeout(() => {
+        subscriber.next(4);
+        subscriber.complete();
+      }, 1000);
+    });
+
+    console.log('just before subscribe');
+    observable.subscribe({
+      next(x) {
+        console.log('got value ' + x);
+      },
+      error(err) {
+        console.error('something wrong occurred: ' + err);
+      },
+      complete() {
+        console.log('done');
+      },
+    });*/
 }
